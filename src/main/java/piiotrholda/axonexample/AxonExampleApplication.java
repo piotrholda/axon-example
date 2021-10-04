1abdc5c5715a76aad8df0bdd4b57522ea7beb903
Piotr Holda <pholda@gmail.com>
2021-10-04 18:02:31 +0200

    Initial commit

---
+++ src/main/java/piiotrholda/axonexample/AxonExampleApplication.java
@@ -0,0 +1,13 @@
+package piiotrholda.axonexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AxonExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AxonExampleApplication.class, args);
+	}
+
+}
