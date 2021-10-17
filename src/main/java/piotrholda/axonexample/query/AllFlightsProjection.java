06ea8f443a87cba323d1413ed5a2cb3ebbc7ccf1
Piotr Holda <pholda@gmail.com>
2021-10-17 22:21:10 +0200

    Use ConcurrentHashMap

--- src/main/java/piotrholda/axonexample/query/AllFlightsProjection.java
+++ src/main/java/piotrholda/axonexample/query/AllFlightsProjection.java
@@ -1,25 +1,20 @@
 package piotrholda.axonexample.query;
 
 import org.axonframework.eventhandling.EventHandler;
-import org.axonframework.queryhandling.QueryGateway;
 import org.axonframework.queryhandling.QueryHandler;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import piotrholda.axonexample.FlightRescheduledEvent;
 import piotrholda.axonexample.FlightScheduledEvent;
-import piotrholda.axonexample.query.FlightSchedule;
-import piotrholda.axonexample.query.ScheduleForBookingQuery;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ConcurrentHashMap;
 
 @Component
 public class AllFlightsProjection {
 
-    private final Map<String, FlightSchedule> flightSchedules = new HashMap<>();
+    private final Map<String, FlightSchedule> flightSchedules = new ConcurrentHashMap<>();
 
     @EventHandler
     public void on(FlightScheduledEvent event) {
@@ -28,7 +23,7 @@ public class AllFlightsProjection {
     }
 
     @EventHandler
-    public void on (FlightRescheduledEvent event) {
+    public void on(FlightRescheduledEvent event) {
         flightSchedules.get(event.getFlightId()).setGateNumber(event.getGateNumber());
     }
 
