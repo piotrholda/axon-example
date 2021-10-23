aada5e45719b1f69c149e01a73b68811b053c20e
Piotr Holda <pholda@gmail.com>
2021-10-23 16:08:32 +0200

    Add spring profiles.

--- src/test/java/piotrholda/axonexample/AxonExampleApplicationTests.java
+++ src/test/java/piotrholda/axonexample/AxonExampleApplicationTests.java
@@ -2,7 +2,9 @@ package piotrholda.axonexample;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class AxonExampleApplicationTests {
 
